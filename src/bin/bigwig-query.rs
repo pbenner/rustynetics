128525a5d9b8329ca5407416306b8ff40b67436d
Philipp Benner <philipp@zeroprobability.org>
2024-08-28 15:24:17 +0200

    2024/08/28-15:24:17 (Linux cray unknown)

---
+++ src/bin/bigwig-query.rs
@@ -0,0 +1,115 @@
+/* Copyright (C) 2024 Philipp Benner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use std::process;
+
+use clap::{Arg, Command};
+
+use rustynetics::bigwig::BigWigReader;
+use rustynetics::bigwig::BigWigFile;
+
+/* -------------------------------------------------------------------------- */
+
+fn query(filename_in: &str, chrom: &str, from: usize, to: usize, bin_size: usize) {
+    // Open the BigWig file
+    let mut reader = BigWigFile::open(filename_in).unwrap_or_else(|err| {
+        eprintln!("Error opening file: {}", err);
+        process::exit(1);
+    });
+
+    // Query the BigWig file
+    for result in reader.query(chrom, from, to, bin_size) {
+        match result {
+            Ok(record) => {
+                println!("{:?}", record);
+            }
+            Err(err) => {
+                eprintln!("Error querying BigWig file: {}", err);
+                process::exit(1);
+            }
+        }
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+fn main() {
+    let matches = Command::new("BigWig Query")
+        .version("1.0")
+        .author("Your Name <your.email@example.com>")
+        .about("Query BigWig files")
+        .arg(
+            Arg::new("input")
+                .help("The input BigWig file")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::new("chrom")
+                .help("The chromosome to query")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::new("from")
+                .help("The start position")
+                .required(true)
+                .index(3),
+        )
+        .arg(
+            Arg::new("to")
+                .help("The end position")
+                .required(true)
+                .index(4),
+        )
+        .arg(
+            Arg::new("binsize")
+                .help("The bin size for the query")
+                .required(true)
+                .index(5),
+        )
+        .get_matches();
+
+    let filename_in = matches.get_one::<String>("input").expect("Input file is required");
+    let chrom = matches.get_one::<String>("chrom").expect("Chromosome is required");
+    let from: usize = matches
+        .get_one::<String>("from")
+        .expect("Start position is required")
+        .parse()
+        .unwrap_or_else(|_| {
+            eprintln!("Invalid start position");
+            process::exit(1);
+        });
+    let to: usize = matches
+        .get_one::<String>("to")
+        .expect("End position is required")
+        .parse()
+        .unwrap_or_else(|_| {
+            eprintln!("Invalid end position");
+            process::exit(1);
+        });
+    let bin_size: usize = matches
+        .get_one::<String>("binsize")
+        .expect("Bin size is required")
+        .parse()
+        .unwrap_or_else(|_| {
+            eprintln!("Invalid bin size");
+            process::exit(1);
+        });
+
+    query(filename_in, chrom, from, to, bin_size);
+
+}
