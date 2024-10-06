ee436b5c0e83e9aec8ddd08d1c6a6531055a9af5
Philipp Benner <philipp@zeroprobability.org>
2024-10-06 10:51:19 +0200

    2024/10/06-10:51:19 (Linux cray unknown)

--- src/track_coverage.rs
+++ src/track_coverage.rs
@@ -18,10 +18,12 @@ use std::fmt;
 use std::io::Write;
 use std::sync::Arc;
 
+use std::io;
+
 /* -------------------------------------------------------------------------- */
 
 // Define an enum to represent all the possible option values
-pub enum OptionTrackCoverage {
+pub enum OptionBamCoverage {
     Logger(Arc<dyn Write>),
     BinningMethod(String),
     BinSize(i64),
@@ -50,33 +52,180 @@ pub enum OptionTrackCoverage {
 
 /* -------------------------------------------------------------------------- */
 
-impl fmt::Display for OptionTrackCoverage {
+impl fmt::Display for OptionBamCoverage {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            OptionTrackCoverage::Logger(_) => write!(f, "Logger option"),
-            OptionTrackCoverage::BinningMethod(s) => write!(f, "Binning Method: {}", s),
-            OptionTrackCoverage::BinSize(size) => write!(f, "Bin Size: {}", size),
-            OptionTrackCoverage::BinOverlap(overlap) => write!(f, "Bin Overlap: {}", overlap),
-            OptionTrackCoverage::NormalizeTrack(s) => write!(f, "Normalize Track: {}", s),
-            OptionTrackCoverage::ShiftReads(arr) => write!(f, "Shift Reads: {:?}", arr),
-            OptionTrackCoverage::PairedAsSingleEnd(b) => write!(f, "Paired as Single End: {}", b),
-            OptionTrackCoverage::PairedEndStrandSpecific(b) => write!(f, "Paired End Strand Specific: {}", b),
-            OptionTrackCoverage::LogScale(b) => write!(f, "Log Scale: {}", b),
-            OptionTrackCoverage::Pseudocounts(arr) => write!(f, "Pseudocounts: {:?}", arr),
-            OptionTrackCoverage::EstimateFraglen(b) => write!(f, "Estimate Fraglen: {}", b),
-            OptionTrackCoverage::FraglenRange(arr) => write!(f, "Fraglen Range: {:?}", arr),
-            OptionTrackCoverage::FraglenBinSize(size) => write!(f, "Fraglen Bin Size: {}", size),
-            OptionTrackCoverage::FilterChroms(v) => write!(f, "Filter Chroms: {:?}", v),
-            OptionTrackCoverage::RemoveFilteredChroms(b) => write!(f, "Remove Filtered Chroms: {}", b),
-            OptionTrackCoverage::FilterMapQ(q) => write!(f, "Filter MapQ: {}", q),
-            OptionTrackCoverage::FilterReadLengths(arr) => write!(f, "Filter Read Lengths: {:?}", arr),
-            OptionTrackCoverage::FilterDuplicates(b) => write!(f, "Filter Duplicates: {}", b),
-            OptionTrackCoverage::FilterStrand(strand) => write!(f, "Filter Strand: {}", strand),
-            OptionTrackCoverage::FilterPairedEnd(b) => write!(f, "Filter Paired End: {}", b),
-            OptionTrackCoverage::FilterSingleEnd(b) => write!(f, "Filter Single End: {}", b),
-            OptionTrackCoverage::SmoothenControl(b) => write!(f, "Smoothen Control: {}", b),
-            OptionTrackCoverage::SmoothenSizes(v) => write!(f, "Smoothen Sizes: {:?}", v),
-            OptionTrackCoverage::SmoothenMin(min) => write!(f, "Smoothen Min: {}", min),
+            OptionBamCoverage::Logger(_) => write!(f, "Logger option"),
+            OptionBamCoverage::BinningMethod(s) => write!(f, "Binning Method: {}", s),
+            OptionBamCoverage::BinSize(size) => write!(f, "Bin Size: {}", size),
+            OptionBamCoverage::BinOverlap(overlap) => write!(f, "Bin Overlap: {}", overlap),
+            OptionBamCoverage::NormalizeTrack(s) => write!(f, "Normalize Track: {}", s),
+            OptionBamCoverage::ShiftReads(arr) => write!(f, "Shift Reads: {:?}", arr),
+            OptionBamCoverage::PairedAsSingleEnd(b) => write!(f, "Paired as Single End: {}", b),
+            OptionBamCoverage::PairedEndStrandSpecific(b) => write!(f, "Paired End Strand Specific: {}", b),
+            OptionBamCoverage::LogScale(b) => write!(f, "Log Scale: {}", b),
+            OptionBamCoverage::Pseudocounts(arr) => write!(f, "Pseudocounts: {:?}", arr),
+            OptionBamCoverage::EstimateFraglen(b) => write!(f, "Estimate Fraglen: {}", b),
+            OptionBamCoverage::FraglenRange(arr) => write!(f, "Fraglen Range: {:?}", arr),
+            OptionBamCoverage::FraglenBinSize(size) => write!(f, "Fraglen Bin Size: {}", size),
+            OptionBamCoverage::FilterChroms(v) => write!(f, "Filter Chroms: {:?}", v),
+            OptionBamCoverage::RemoveFilteredChroms(b) => write!(f, "Remove Filtered Chroms: {}", b),
+            OptionBamCoverage::FilterMapQ(q) => write!(f, "Filter MapQ: {}", q),
+            OptionBamCoverage::FilterReadLengths(arr) => write!(f, "Filter Read Lengths: {:?}", arr),
+            OptionBamCoverage::FilterDuplicates(b) => write!(f, "Filter Duplicates: {}", b),
+            OptionBamCoverage::FilterStrand(strand) => write!(f, "Filter Strand: {}", strand),
+            OptionBamCoverage::FilterPairedEnd(b) => write!(f, "Filter Paired End: {}", b),
+            OptionBamCoverage::FilterSingleEnd(b) => write!(f, "Filter Single End: {}", b),
+            OptionBamCoverage::SmoothenControl(b) => write!(f, "Smoothen Control: {}", b),
+            OptionBamCoverage::SmoothenSizes(v) => write!(f, "Smoothen Sizes: {:?}", v),
+            OptionBamCoverage::SmoothenMin(min) => write!(f, "Smoothen Min: {}", min),
         }
     }
 }
+
+/* -------------------------------------------------------------------------- */
+
+// Define the BamCoverageConfig struct
+pub struct BamCoverageConfig {
+    pub logger: Arc<dyn Write>,
+    pub binning_method: String,
+    pub bin_size: i64,
+    pub bin_overlap: i64,
+    pub normalize_track: String,
+    pub shift_reads: [i64; 2],
+    pub paired_as_single_end: bool,
+    pub paired_end_strand_specific: bool,
+    pub log_scale: bool,
+    pub pseudocounts: [f64; 2],
+    pub estimate_fraglen: bool,
+    pub fraglen_range: [i64; 2],
+    pub fraglen_bin_size: i64,
+    pub filter_chroms: Vec<String>,
+    pub filter_map_q: i64,
+    pub filter_read_lengths: [i64; 2],
+    pub filter_duplicates: bool,
+    pub filter_strand: u8,
+    pub filter_paired_end: bool,
+    pub filter_single_end: bool,
+    pub remove_filtered_chroms: bool,
+    pub smoothen_control: bool,
+    pub smoothen_sizes: Vec<i64>,
+    pub smoothen_min: f64,
+}
+
+/* -------------------------------------------------------------------------- */
+
+// Function to insert OptionBamCoverage into BamCoverageConfig
+impl BamCoverageConfig {
+    pub fn insert_option(&mut self, option: OptionBamCoverage) {
+        match option {
+            OptionBamCoverage::Logger(logger) => {
+                self.logger = logger;
+            }
+            OptionBamCoverage::BinningMethod(method) => {
+                self.binning_method = method;
+            }
+            OptionBamCoverage::BinSize(size) => {
+                self.bin_size = size;
+            }
+            OptionBamCoverage::BinOverlap(overlap) => {
+                self.bin_overlap = overlap;
+            }
+            OptionBamCoverage::NormalizeTrack(track) => {
+                self.normalize_track = track;
+            }
+            OptionBamCoverage::ShiftReads(reads) => {
+                self.shift_reads = reads;
+            }
+            OptionBamCoverage::PairedAsSingleEnd(paired) => {
+                self.paired_as_single_end = paired;
+            }
+            OptionBamCoverage::PairedEndStrandSpecific(strand_specific) => {
+                self.paired_end_strand_specific = strand_specific;
+            }
+            OptionBamCoverage::LogScale(log_scale) => {
+                self.log_scale = log_scale;
+            }
+            OptionBamCoverage::Pseudocounts(pseudocounts) => {
+                self.pseudocounts = pseudocounts;
+            }
+            OptionBamCoverage::EstimateFraglen(estimate) => {
+                self.estimate_fraglen = estimate;
+            }
+            OptionBamCoverage::FraglenRange(range) => {
+                self.fraglen_range = range;
+            }
+            OptionBamCoverage::FraglenBinSize(size) => {
+                self.fraglen_bin_size = size;
+            }
+            OptionBamCoverage::FilterChroms(chroms) => {
+                self.filter_chroms = chroms;
+            }
+            OptionBamCoverage::FilterMapQ(map_q) => {
+                self.filter_map_q = map_q;
+            }
+            OptionBamCoverage::FilterReadLengths(read_lengths) => {
+                self.filter_read_lengths = read_lengths;
+            }
+            OptionBamCoverage::FilterDuplicates(duplicates) => {
+                self.filter_duplicates = duplicates;
+            }
+            OptionBamCoverage::FilterStrand(strand) => {
+                self.filter_strand = strand;
+            }
+            OptionBamCoverage::FilterPairedEnd(paired_end) => {
+                self.filter_paired_end = paired_end;
+            }
+            OptionBamCoverage::FilterSingleEnd(single_end) => {
+                self.filter_single_end = single_end;
+            }
+            OptionBamCoverage::RemoveFilteredChroms(remove) => {
+                self.remove_filtered_chroms = remove;
+            }
+            OptionBamCoverage::SmoothenControl(smoothen) => {
+                self.smoothen_control = smoothen;
+            }
+            OptionBamCoverage::SmoothenSizes(sizes) => {
+                self.smoothen_sizes = sizes;
+            }
+            OptionBamCoverage::SmoothenMin(min) => {
+                self.smoothen_min = min;
+            }
+        }
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+// Function to provide default configuration
+impl BamCoverageConfig {
+    pub fn default() -> Self {
+        BamCoverageConfig {
+            logger: Arc::new(io::sink()), // Discard logger output
+            binning_method: String::from("simple"),
+            bin_size: 10,
+            bin_overlap: 0,
+            normalize_track: String::new(),
+            shift_reads: [0, 0],
+            paired_as_single_end: false,
+            paired_end_strand_specific: false,
+            log_scale: false,
+            pseudocounts: [0.0, 0.0],
+            estimate_fraglen: false,
+            fraglen_range: [-1, -1],
+            fraglen_bin_size: 10,
+            filter_chroms: Vec::new(),
+            filter_map_q: 0,
+            filter_read_lengths: [0, 0],
+            filter_duplicates: false,
+            filter_strand: b'*',
+            filter_paired_end: false,
+            filter_single_end: false,
+            remove_filtered_chroms: false,
+            smoothen_control: false,
+            smoothen_sizes: Vec::new(),
+            smoothen_min: 20.0,
+        }
+    }
+}
+
